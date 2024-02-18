6efeb74090227059886e584cd1606a3b9d3f90ff
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-18 19:50:32 +0300

    resources

--- src/main/java/com/iw/cstat/App.java
+++ src/main/java/com/iw/cstat/App.java
@@ -2,11 +2,14 @@ package com.iw.cstat;
 
 import com.iw.cstat.page.HomePage;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class App {
     public static void main(String[] args) {
         final Page home = new HomePage();
-        Javalin.create()
+        Javalin.create(cfg -> {
+                    cfg.staticFiles.add("/assets/public", Location.CLASSPATH);
+                })
                 .get("/", ctx -> ctx.html(home.render()))
                 .start(8080);
     }
