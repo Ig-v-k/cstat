075193fd8f206fbbe43d966b9463cdeb9adf4955
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-22 22:03:51 +0300

    type array serialize

--- src/main/java/com/iw/cstat/cstat/JsonCStats.java
+++ src/main/java/com/iw/cstat/cstat/JsonCStats.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.iw.cstat.*;
 import com.iw.cstat.atttributes.SimpleAttributes;
 import com.iw.cstat.data.SimpleData;
+import com.iw.cstat.institution.SimpleInstitution;
 import com.iw.cstat.jsonapi.SimpleJsonApi;
 import com.iw.cstat.links.SimpleLinks;
 import com.iw.cstat.meta.SimpleMeta;
@@ -41,6 +42,7 @@ public final class JsonCStats implements CStats {
                 .registerTypeAdapter(Attributes.class, new Serialize<>(SimpleAttributes.class))
                 .registerTypeAdapter(Relationships.class, new Serialize<>(SimpleRelationships.class))
                 .registerTypeAdapter(Resources.class, new Serialize<>(SimpleResources.class))
+                .registerTypeAdapter(Institution.class, new Serialize<>(SimpleInstitution.class))
                 .registerTypeAdapter(CStats.class, new Serialize<>(JsonCStats.class))
                 .create()
                 .fromJson(str, CStats.class);
--- src/main/java/com/iw/cstat/facet/Facet1681.java
+++ src/main/java/com/iw/cstat/facet/Facet1681.java
@@ -5,6 +5,7 @@ import com.iw.cstat.CStat;
 import com.iw.cstat.CStats;
 import com.iw.cstat.Facet;
 import com.iw.cstat.api.DatasetOf;
+import com.iw.cstat.api.InstitutionOf;
 import com.iw.cstat.api.ResourcesAPI;
 import com.iw.cstat.cstat.JsonCStats;
 import com.iw.cstat.dat.DateFormat;
@@ -42,7 +43,10 @@ public final class Facet1681 implements Facet<DivTag> {
                 "yyyy-MM-dd'T'HH:mm:ss'Z'",
                 cStat.data().attributes().verified(),
                 cStat.meta().language()).text();
+        final CStat institution = cStat.from(new RequestRes(
+                new InstitutionOf(cStat.data().relationships().institution().data().id())).body());
         return p(join(
+                institution.data().attributes().title(), " • ",
                 "Polish", " • ",
                 verified, " • "
         ));
