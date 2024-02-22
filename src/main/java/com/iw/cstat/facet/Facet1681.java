c2f4b9a6c76200156126e7791df3da8e4fa36388
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-22 23:28:09 +0300

    statistic

--- src/main/java/com/iw/cstat/facet/Facet1681.java
+++ src/main/java/com/iw/cstat/facet/Facet1681.java
@@ -7,12 +7,15 @@ import com.iw.cstat.Facet;
 import com.iw.cstat.api.DatasetOf;
 import com.iw.cstat.api.InstitutionOf;
 import com.iw.cstat.api.ResourcesAPI;
+import com.iw.cstat.cstat.JsonCStat;
 import com.iw.cstat.cstat.JsonCStats;
 import com.iw.cstat.dat.DateFormat;
 import com.iw.cstat.res.RequestRes;
 import j2html.tags.Tag;
 import j2html.tags.specialized.DivTag;
 import j2html.tags.specialized.PTag;
+
+import java.util.Calendar;
 
 import static j2html.TagCreator.*;
 
@@ -28,13 +31,18 @@ public final class Facet1681 implements Facet<DivTag> {
     public Tag<DivTag> tag() {
         final CStats cStats = new JsonCStats();
         final API api1681 = new DatasetOf(1681);
-        final CStats mans = cStats.from(new RequestRes(new ResourcesAPI(api1681, "title[phrase]=m%C4%99skie", "created[gte]=2023")).body());
-        final CStats woman = cStats.from(new RequestRes(new ResourcesAPI(api1681, "title[phrase]=%C5%BCe%C5%84skie", "created[gte]=2023")).body());
+        final CStats mans = cStats.from(new RequestRes(new ResourcesAPI(api1681, "title[phrase]=m%C4%99skie", "created[gte]=2024")).body());
+        final CStats woman = cStats.from(new RequestRes(new ResourcesAPI(api1681, "title[phrase]=%C5%BCe%C5%84skie", "created[gte]=2024")).body());
         return div(
                 header(
                         h1(cStat.data().attributes().title()),
                         subline(cStat)),
-                div(rawHtml(cStat.data().attributes().notes()))
+                div(
+                        rawHtml(cStat.data().attributes().notes()),
+                        h3("Statystyka:"),
+                        p(join(b("NOWAK"), " - 99310")),
+                        p(join(b("NOWAK"), " - 100906"))
+                )
         );
     }
 
