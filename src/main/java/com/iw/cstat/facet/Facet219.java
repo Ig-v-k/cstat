22da66feb251dbd3a85389551a024fe6eb5c85c4
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-03 14:31:22 +0300

    219 basic page

--- src/main/java/com/iw/cstat/facet/Facet219.java
+++ src/main/java/com/iw/cstat/facet/Facet219.java
@@ -2,10 +2,15 @@ package com.iw.cstat.facet;
 
 import com.iw.cstat.CStat;
 import com.iw.cstat.Facet;
+import com.iw.cstat.api.InstitutionOf;
+import com.iw.cstat.dat.DateFormat;
+import com.iw.cstat.res.RequestRes;
 import j2html.tags.Tag;
 import j2html.tags.specialized.DivTag;
+import j2html.tags.specialized.PTag;
 
-import static j2html.TagCreator.div;
+import static j2html.TagCreator.*;
+import static j2html.TagCreator.rawHtml;
 
 public final class Facet219 implements Facet<DivTag> {
 
@@ -17,6 +22,24 @@ public final class Facet219 implements Facet<DivTag> {
 
     @Override
     public Tag<DivTag> tag() {
-        return div("Page");
+        return div(
+                header(
+                        h1(cStat.data().attributes().title()),
+                        subline(cStat)),
+                div(
+                        rawHtml(cStat.data().attributes().notes())));
+    }
+
+    private static PTag subline(final CStat cStat) {
+        final String verified = new DateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss'Z'",
+                cStat.data().attributes().verified(),
+                cStat.meta().language()).text();
+        final CStat institution = cStat.from(new RequestRes(
+                new InstitutionOf(cStat.data().relationships().institution().data().id())).body());
+        return p(join(
+                a(institution.data().attributes().title()).withHref(institution.data().attributes().website()), " • ",
+                verified
+        ));
     }
 }
