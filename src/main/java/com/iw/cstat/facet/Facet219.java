046797196c1ff791e27eb1eeb95d047ec7727c0e
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-03 18:19:18 +0300

    rewrite for template structure:

--- src/main/java/com/iw/cstat/facet/Facet219.java
+++ src/main/java/com/iw/cstat/facet/Facet219.java
@@ -24,27 +24,10 @@ public final class Facet219 implements Facet<DivTag> {
     public Tag<DivTag> tag() {
         final String slug = cStat.data().attributes().slug();
         return div(
-                header(
-                        h1(cStat.data().attributes().title()),
-                        subline(cStat)),
-                div(
-                        rawHtml(cStat.data().attributes().notes()),
-                        ul(
-                                li(a("Imiona zenskie - imie perwsze").withHref(slug + "/imiona-zenskie-imie-pierwsze")),
-                                li(a("Imiona meskie - imie perwsze").withHref(slug + "/imiona-meskie-imie-pierwsze"))
-                        )));
-    }
-
-    private static PTag subline(final CStat cStat) {
-        final String verified = new DateFormat(
-                "yyyy-MM-dd'T'HH:mm:ss'Z'",
-                cStat.data().attributes().verified(),
-                cStat.meta().language()).text();
-        final CStat institution = cStat.from(new RequestRes(
-                new InstitutionOf(cStat.data().relationships().institution().data().id())).body());
-        return p(join(
-                a(institution.data().attributes().title()).withHref(institution.data().attributes().website()), " • ",
-                verified
-        ));
+                rawHtml(cStat.data().attributes().notes()),
+                ul(
+                        li(a("Imiona zenskie - imie perwsze").withHref(slug + "/imiona-zenskie-imie-pierwsze")),
+                        li(a("Imiona meskie - imie perwsze").withHref(slug + "/imiona-meskie-imie-pierwsze"))
+                ));
     }
 }
--- src/main/java/com/iw/cstat/page/Page219.java
+++ src/main/java/com/iw/cstat/page/Page219.java
@@ -4,9 +4,14 @@ import com.iw.cstat.CStat;
 import com.iw.cstat.Page;
 import com.iw.cstat.Res;
 import com.iw.cstat.api.DatasetOf;
+import com.iw.cstat.api.InstitutionOf;
 import com.iw.cstat.cstat.JsonCStat;
+import com.iw.cstat.dat.DateFormat;
 import com.iw.cstat.facet.Facet219;
 import com.iw.cstat.res.RequestRes;
+import j2html.tags.specialized.PTag;
+
+import static j2html.TagCreator.*;
 
 public final class Page219 implements Page {
     @Override
@@ -16,7 +21,15 @@ public final class Page219 implements Page {
         return new TmplPage(
                 cstat.data().attributes().title(),
                 cstat.data().attributes().title(),
-                "",
+                subline(cstat),
                 new Facet219(cstat)).render();
+    }
+
+    private static String subline(final CStat cStat) {
+        final String verified = new DateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss'Z'",
+                cStat.data().attributes().verified(),
+                cStat.meta().language()).text();
+        return verified;
     }
 }
