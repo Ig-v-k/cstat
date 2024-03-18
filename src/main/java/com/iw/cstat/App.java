472139ae64db551fcb9837a82ed59b5b956ec53c
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-18 22:16:05 +0300

    format

--- src/main/java/com/iw/cstat/App.java
+++ src/main/java/com/iw/cstat/App.java
@@ -8,9 +8,7 @@ public class App {
     public static void main(String[] args) {
         final Page home = new HomePage();
         final Page p1861 = new Page1681();
-        Javalin.create(cfg -> {
-                    cfg.staticFiles.add("/assets/public", Location.CLASSPATH);
-                })
+        Javalin.create(cfg -> cfg.staticFiles.add("/assets/public", Location.CLASSPATH))
                 .get("/", ctx -> ctx.html(home.render()))
                 .get("/nazwiska-osob-zyjacych-wystepujace-w-rejestrze-pesel", ctx -> ctx.html(p1861.render()))
                 .get("/imiona-nadawane-dzieciom-w-polsce", ctx -> ctx.html(new Page219(ctx.queryParamMap()).render()))
